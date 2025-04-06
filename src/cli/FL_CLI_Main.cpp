125ce617c61a8458d6a5a3842c747389b7933a0a
Sujit Malde <sujit@comind.io>
2025-04-06 11:52:54 +0100

    Starting development of CLI tool

---
+++ src/cli/FL_CLI_Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <getopt.h>
+
+
+
+int main(int argc, char **argv)
+{
+    static struct option long_opts[] = {
+        {"help", no_argument, nullptr, 'h'},
+        {nullptr, 0, nullptr, 0}
+    };
+
+    int opt;
+    
+    
+    return 0;
+}
