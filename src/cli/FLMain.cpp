abe9fec1c02d53cfc3340df0c1215a331aa4001c
Sujit Malde <samman30@live.com>
2025-04-06 16:14:35 +0100

    Imported UserData class into CLI world

--- src/cli/FLMain.cpp
+++ src/cli/FLMain.cpp
@@ -1,23 +1,22 @@
 #include <getopt.h>
-#include <stdio.h>     /* for printf */
-#include <stdlib.h>    /* for exit */
-
 #include <iostream>
 
-constexpr const char *opts {"abc:d:h012"};
+#include <base/UserData.h>
+
+constexpr const char *opts {"h012"};
+
+Fixlator::UserData userdata;
 
 static struct option long_options[] = {
-   {"add",     required_argument, 0,  0 },
-   {"append",  no_argument,       0,  0 },
-   {"delete",  required_argument, 0,  0 },
-   {"verbose", no_argument,       0,  0 },
-   {"create",  required_argument, 0, 'c'},
-   {"file",    required_argument, 0,  0 },
-   {"help",    no_argument,       0, 'h'},
-   {0,         0,                 0,  0 }
+    {"add",     required_argument, 0,  0 },
+    {"append",  no_argument,       0,  0 },
+    {"delete",  required_argument, 0,  0 },
+    {"verbose", no_argument,       0,  0 },
+    {"create",  required_argument, 0, 'c'},
+    {"file",    required_argument, 0,  0 },
+    {"help",    no_argument,       0, 'h'},
+    {0,         0,                 0,  0 }
 };
-
-
 
 void print_help(char *proc_name)
 {
@@ -25,74 +24,79 @@ void print_help(char *proc_name)
     std::cout << " " << proc_name << " [options]\n";
     std::cout << "Options:\n";
     std::cout << " -h, --help        displays this help text and exit\n";
-    std::cout << "\nCalling " << proc_name << " without args will run CLI translation by default\n";
+    std::cout << "\nCalling " << proc_name << " without args will translate by default\n";
     std::cout << std::endl;
 }
 
-int main(int argc, char **argv)
+int translate_raw_fix()
 {
-   int c {};
-   int digit_optind {0};
+    //for (std::string line; std::getline(std::cin, line);) {
+    //    std::cout << line << std::endl;
+    //}
+    //
+    
+    userdata.SaveData(std::cin);
+    std::cout << userdata.GetNumSavedMessages();
 
-   while (1) {
-       int this_option_optind {optind ? optind : 1};
-       int option_index {0};
+    return 0;
+}
 
-       c = getopt_long(argc, argv, opts, long_options, &option_index);
-       if (c == -1)
-           break;
+int main(int argc, char **argv)
+{ 
+    char *in_buf {};
+    int c {-1};
+    int digit_optind {0};
 
-       switch (c) {
-       case 0:
-           printf("option %s", long_options[option_index].name);
-           if (optarg)
-               printf(" with arg %s", optarg);
-           printf("\n");
-           break;
+    while (1) {
+        int this_option_optind {optind ? optind : 1};
+        int option_index {0};
 
-       case '0':
-       case '1':
-       case '2':
-           if (digit_optind != 0 && digit_optind != this_option_optind)
-             printf("digits occur in two different argv-elements.\n");
-           digit_optind = this_option_optind;
-           printf("option %c\n", c);
-           break;
+        c = getopt_long(argc, argv, opts, long_options, &option_index);
+        if (c == -1) {
+            std::cout << "\nNo argument provided" << std::endl;
+            translate_raw_fix();
+            break;
+        }
 
-       case 'a':
-           printf("option a\n");
-           break;
+        switch (c) {
+        case 0:
+            std::cout << "option " << long_options[option_index].name;
+            if (optarg) {
+                std::cout << " with arg " << optarg;
+            }
+            std::cout << std::endl;
+            break;
 
-       case 'b':
-           printf("option b\n");
-           break;
+        case '0':
+        case '1':
+        case '2':
+            if (digit_optind != 0 && digit_optind != this_option_optind) {
+                std::cout << "digits occur in two different argv-elements.\n";
+            }
+            digit_optind = this_option_optind;
+            std::cout << "option " << (char)c << std::endl;
+            break;
 
-       case 'c':
-           printf("option c with value '%s'\n", optarg);
-           break;
+        case 'h':
+            print_help(argv[0]);
+            break;
 
-       case 'd':
-           printf("option d with value '%s'\n", optarg);
-           break;
+        case '?':
+            std::cout << "hello";
+            break;
 
-       case 'h':
-           print_help(argv[0]);
-           break;
+        default:
+            std::cout << "?? getopt returned character code 0 " << (char)c << " ??" << std::endl;
+        }
+    }
 
-       case '?':
-           break;
+    if (optind < argc) {
+        std::cout << "non-option ARGV-elements: ";
+        while (optind < argc) {
+            std::cout << argv[optind++] << " ";
+        }
+        std::cout << std::endl;
+    }
 
-       default:
-           printf("?? getopt returned character code 0%o ??\n", c);
-       }
-   }
-
-   if (optind < argc) {
-       printf("non-option ARGV-elements: ");
-       while (optind < argc)
-           printf("%s ", argv[optind++]);
-       printf("\n");
-   }
-
-   exit(EXIT_SUCCESS);
+    return 0;
 }
